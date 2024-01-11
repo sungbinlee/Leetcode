979b610da5fc7ff2cba31f372738ebf237e4e257
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-01-11 13:15:30 +0900

    Time: 36 ms (81.64%), Space: 17.4 MB (13.80%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        while nums.count(val):
+            nums.remove(val)
+            
+        return len(nums)

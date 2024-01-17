37e0c09d56172e53e7d7225d598f544c1baed71c
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-01-17 17:21:49 +0900

    Time: 52 ms (96.23%), Space: 18.6 MB (21.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map = {}
+        nums_hash = {}
         
         for i, num in enumerate(nums):
-            nums_map[num] = i
-            
+            nums_hash[num] = i
+        
         for i, num in enumerate(nums):
-            if target-num in nums_map and i != nums_map[target-num]:
-                return [i, nums_map[target-num]]
+            if target - num in nums_hash and i != nums_hash[target-num]:
+                return [i, nums_hash[target-num]]
+            
+            

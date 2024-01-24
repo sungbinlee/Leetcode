dd13e9660cd9a037b1a7946ca0c6143ddf97a99a
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-01-24 21:14:28 +0900

    Time: 271 ms (59.52%), Space: 18.7 MB (98.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,32 +3,32 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
-        cols = len(grid)
-        rows = len(grid[0])
-        visited = [[False] * rows for _ in range(cols)]
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = [[False]*cols for _ in range(rows)]
         
         def bfs(x, y):
-            dx = [-1, 1, 0, 0]
-            dy = [0, 0, -1, 1]
             visited[x][y] = True
             q = deque()
             q.append((x, y))
             
+            dx = [-1, 1, 0, 0]
+            dy = [0, 0, -1, 1]
+            
             while q:
-                cur_x, cur_y = q.popleft()
+                curr_x, curr_y = q.popleft()
                 for i in range(4):
-                    next_x = cur_x + dx[i]
-                    next_y = cur_y + dy[i]
-                    if next_x >= 0 and next_x < cols and next_y >= 0 and next_y < rows:
+                    next_x = curr_x + dx[i]
+                    next_y = curr_y + dy[i]
+                    if next_x >= 0 and next_x < rows and next_y >= 0 and next_y < cols:
                         if grid[next_x][next_y] == "1" and not visited[next_x][next_y]:
                             visited[next_x][next_y] = True
                             q.append((next_x, next_y))
             
-        for i in range(cols):
-            for j in range(rows):
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == "1" and not visited[i][j]:
                     bfs(i, j)
                     count += 1
-        
-        
+            
         return count

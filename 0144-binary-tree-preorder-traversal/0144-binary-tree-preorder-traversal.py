05c288d0a183664f2d95ee3d2acd900fc5c9f51f
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-01-24 23:07:48 +0900

    Time: 37 ms (61.10%), Space: 16.5 MB (60.02%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        answer = []
+        
+        def dfs(root):
+            if root is None:
+                return None
+            
+            answer.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        
+        dfs(root)
+        
+        return answer

0762f8006b9b89eb116ace8a3aab0d23073e62b4
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-01-24 13:20:35 +0900

    Time: 45 ms (92.98%), Space: 16.7 MB (59.39%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,23 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        
+        L = [''] * numRows
+        
+        index, step = 0, 1
+        
+        for v in s:
+            L[index] += v
+            if index == 0:
+                step = 1
+            elif index == numRows-1:
+                step = -1
+            index += step
+            
+        return ''.join(L)
+                
+            
+            

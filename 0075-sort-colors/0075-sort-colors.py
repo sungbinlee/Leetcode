5b0860d7b579468a62e9f6506a4b69eba0ac0f3b
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-02-01 02:19:40 +0900

    Time: 39 ms (59.73%), Space: 16.5 MB (62.50%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -8,8 +8,15 @@ class Solution:
                 for j in range(i, 0, -1):
                     if array[j] < array[j-1]:
                         array[j-1], array[j] = array[j], array[j-1]
+        
+        def buble_sort(array):
+            for i in range(len(array) - 1):
+                for j in range(len(array) - i - 1):
+                    if array[j] > array[j+1]:
+                        array[j], array[j+1] = array[j+1], array[j]
+                
                         
-        insertion_sort(nums)
+        buble_sort(nums)
                 
         
         

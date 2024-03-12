73d47ce5e23fa106bb2622190c4b447fa5d98c79
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-03-12 23:41:09 +0900

    Time: 892 ms (62.51%), Space: 32.2 MB (17.41%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,11 +1,22 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # temperatures 어레이는 일일온도 담겨있음
+        # 얼마나 기다려야 따뜻해지는지 담긴 배열 반환, 따뜻해질 예정 없으면 값 0 
+        
         answer = [0] * len(temperatures)
+        
         stack = []
-        for curr_d, curr_t in enumerate(temperatures):
-            while stack and stack[-1][1] < curr_t:
+        
+        for cur_d, cur_t in enumerate(temperatures):
+            while stack and stack[-1][1] < cur_t:
                 prev_d, _ = stack.pop()
-                answer[prev_d] = curr_d - prev_d
-            stack.append((curr_d, curr_t))
-
+                answer[prev_d] = cur_d - prev_d 
+            
+            stack.append((cur_d, cur_t))
+            
         return answer
+        
+        
+        
+        
+       

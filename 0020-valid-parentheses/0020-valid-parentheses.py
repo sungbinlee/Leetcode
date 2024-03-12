cc33280872b52f77b16d45a01cb6647d68458e33
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-03-12 23:38:57 +0900

    Time: 36 ms (51.98%), Space: 16.6 MB (79.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,12 +3,13 @@ class Solution:
         stack = []
         
         for p in s:
-            if p == '(':
-                stack.append(')')
-            elif p == '[':
-                stack.append(']')
-            elif p == '{':
-                stack.append('}')
+            if p == "(":
+                stack.append(")")
+            elif p == "{":
+                stack.append("}")
+            elif p == "[":
+                stack.append("]")
             elif not stack or stack.pop() != p:
                 return False
+            
         return not stack

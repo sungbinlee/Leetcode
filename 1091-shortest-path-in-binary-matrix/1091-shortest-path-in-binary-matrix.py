abd811f85279cd83797d7495ce414ce165eca0c7
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-03-20 22:12:14 +0900

    Time: 373 ms (91.34%), Space: 16.9 MB (91.10%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,30 +5,31 @@ class Solution:
         shortest_path = -1
         rows = len(grid)
         cols = len(grid[0])
-        queue = deque()
-        queue.append((0, 0, 1))
-        visited = [[False]*cols for _ in range(rows)]
+        visited = [[False] * cols for _ in range(rows)]
+        q = deque()
+        q.append((0, 0, 1))
         visited[0][0] = True
         
-        delta = [(1, 0), (-1, 0), (0, 1), (0, -1),
-                (1, 1), (1, -1), (-1, 1), (-1, -1)]
-        
-        if grid[0][0] == 1 or grid[rows -1][cols-1] == 1:
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
             return shortest_path
         
-        while queue:
-            cur_x, cur_y, cur_len = queue.popleft()
+        delta = [(1, 0), (-1, 0), (0, 1), (0, -1),
+                (1, 1), (-1, 1), (1, -1), (-1, -1)]
+        
+        while q:
+            cur_x, cur_y, cur_len = q.popleft()
             
-            if cur_x == rows - 1 and cur_y == cols -1:
+            if cur_x == rows - 1 and cur_y == cols - 1:
                 shortest_path = cur_len
                 break
-
+                
             for dx, dy in delta:
                 next_x = cur_x + dx
                 next_y = cur_y + dy
-                if next_x >= 0 and next_x < rows and next_y >=0 and next_y < cols:
+                if next_x >= 0 and next_x < rows and next_y >= 0 and next_y < cols:
                     if grid[next_x][next_y] == 0 and not visited[next_x][next_y]:
-                        queue.append((next_x, next_y, cur_len + 1))
+                        q.append((next_x, next_y, cur_len + 1))
                         visited[next_x][next_y] = True
-                        
+                
+        
         return shortest_path

ec378c7fe358d4573aa5e9951e84660038680fd0
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-03-28 17:54:50 +0900

    Time: 249 ms (54.24%), Space: 19 MB (61.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,31 +5,36 @@ class Solution:
         cnt = 0
         rows = len(grid)
         cols = len(grid[0])
-        visited = [[False]*cols for _ in range(rows)]
-
+        
+        visited = [[False] * cols for _ in range(rows)]
+        
+        dx = [1, -1, 0, 0]
+        dy = [0, 0, 1, -1]
+        
+        def is_range(x, y):
+            if 0 <= x < rows and 0 <= y < cols:
+                return True
+            else:
+                return False
         
         def bfs(x, y):
-            dx = [-1, 1, 0, 0]
-            dy = [0, 0, -1, 1]
-            
             q = deque()
             q.append((x, y))
             visited[x][y] = True
             
             while q:
-                cur_x, cur_y = q.popleft()
+                cur_x, cur_y = q.pop()
                 for i in range(4):
-                    next_x = cur_x + dx[i]
-                    next_y = cur_y + dy[i]
-                    if next_x >= 0 and next_x < rows and next_y >= 0 and next_y < cols:
-                        if not visited[next_x][next_y] and grid[next_x][next_y] == "1":
-                            visited[next_x][next_y] = True
-                            q.append((next_x, next_y))
+                    next_x, next_y = cur_x + dx[i], cur_y + dy[i]
+                    if is_range(next_x, next_y) and not visited[next_x][next_y] and grid[next_x][next_y] == "1":
+                        q.append((next_x, next_y))
+                        visited[next_x][next_y] = True
+                    
         
-        for x in range(rows):
-            for y in range(cols):
-                if grid[x][y] == "1" and not visited[x][y]:
-                    bfs(x, y)
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and not visited[i][j]:
+                    bfs(i, j)
                     cnt += 1
-        
+                    
         return cnt

c00bb0b7be47e778c8583c892311e96d784ea933
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-03-30 16:27:15 +0900

    Time: 128 ms (9.71%), Space: 17 MB (42.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,38 +1,40 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        # 백트래킹으로 완전탐색
+        # - 한줄당 하나씩 
+        # 수를 둘때마다 대각선 및 가로 확인
         def is_valid(x, y):
-            # 가로세로
+            # 가로 세로 확인
             for i in range(n):
-                if board[x][i] == 'Q' or board[i][y] == 'Q':
+                if board[x][i] == "Q" or board[i][y] == "Q":
                     return False
                 
-            # 대각선 체크 
+            # 대각선 확인
             for i in range(n):
                 for j in range(n):
-                    if (i + j == x + y or i - j == x - y) and board[i][j] == 'Q':
+                    if board[i][j] == "Q" and (i + j == x + y or i - j == x - y):
                         return False
                     
             return True
-                    
+        
+        
         def backtrack(row):
-            # base case
+            # basecase
             if row == n:
                 ans.append([''.join(row) for row in board])
                 return
             
             for col in range(n):
                 if is_valid(row, col):
-                    board[row][col] = 'Q'
+                    board[row][col] = "Q"
                     backtrack(row + 1)
-                    board[row][col] = '.'
+                    board[row][col] = "."
             
         
-        board = [['.']*n for _ in range(n)]
+        board = [["."] * n for _ in range(n)]
         ans = []
         backtrack(0)
-        return ans
-        
-            
-            
 
+        
+        return ans
         

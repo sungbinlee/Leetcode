4efed904f66cd24ced6928ea8fd71b8446bfed6f
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-01 00:49:50 +0900

    Time: 82 ms (51.37%), Space: 16.8 MB (82.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,28 +1,12 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        sorted_num = sorted(nums1 + nums2)
         
-        sorted_nums = []
-        
-        i, j = 0, 0
-        while i < len(nums1) and j <len(nums2):
-            if nums1[i] < nums2[j]:
-                sorted_nums.append(nums1[i])
-                i += 1
-            else:
-                sorted_nums.append(nums2[j])
-                j += 1
-                
-        while i < len(nums1):
-            sorted_nums.append(nums1[i])
-            i += 1
-            
-        while j < len(nums2):
-            sorted_nums.append(nums2[j])
-            j += 1
-        
-        total_len = len(sorted_nums)
+        total_len = len(sorted_num)
         
         if total_len % 2 == 1:
-            return sorted_nums[len(sorted_nums) // 2]
+            answer = sorted_num[len(sorted_num) // 2]
         else:
-            return (sorted_nums[(len(sorted_nums) // 2) - 1] + sorted_nums[(len(sorted_nums) // 2)]) / 2
+            answer = (sorted_num[(len(sorted_num) // 2) - 1] + sorted_num[len(sorted_num) // 2]) / 2
+        
+        return answer

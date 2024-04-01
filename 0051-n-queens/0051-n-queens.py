ac4e39152c1a24ac064bc20c5d990ac39b48ab35
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-01 20:45:53 +0900

    Time: 136 ms (9.24%), Space: 17.1 MB (42.76%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,13 +1,10 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        # 백트래킹으로 완전탐색
-        # 수를 둘때마다 가로 세로 대각선 확인
         def is_valid(x, y):
             for i in range(n):
                 if board[x][i] == "Q" or board[i][y] == "Q":
                     return False
-            
-            
+                
             for i in range(n):
                 for j in range(n):
                     if board[i][j] == "Q" and (i + j == x + y or i - j == x - y):
@@ -16,18 +13,20 @@ class Solution:
         
         
         def backtrack(row):
+            # basecase
             if row == n:
                 answer.append([''.join(row) for row in board])
                 return
-                
+            
             for col in range(n):
                 if is_valid(row, col):
                     board[row][col] = "Q"
                     backtrack(row + 1)
                     board[row][col] = "."
-                
+        
         answer = []
         board = [['.'] * n for _ in range(n)]
         backtrack(0)
         
         return answer
+        

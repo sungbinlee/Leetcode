41afea8bf5e4c636a9f36974288ec5319932e871
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-01 21:43:11 +0900

    Time: 382 ms (39.04%), Space: 18.6 MB (41.01%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
-from heapq import heapify, heappush, heappop
+from heapq import heappush, heappop
+
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         # 그래프 구현
         graph = defaultdict(list)
+        
         for time in times:
             graph[time[0]].append((time[2], time[1]))
             
@@ -22,10 +24,10 @@ class Solution:
                     heappush(pq, (next_cost, next_node))
             
         # 조건 확인
-        print(costs)
-        for i in range(1, n+1):
+        for i in range(1, n + 1):
             if i not in costs:
                 return -1
         
         # 최대값 반환
         return max(costs.values())
+        

1edbdbcce9d3c71b24b1461cf677f6a28ec114a4
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-02 00:12:52 +0900

    Time: 449 ms (53.02%), Space: 16.9 MB (91.10%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,41 +1,39 @@
 from collections import deque
 
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # 좌상단 우하단으로 이동해야함
-        # 대각선으로 이동가능
+        def in_range(x, y):
+            return 0 <= x < len(grid) and 0 <= y < len(grid)
+        
+        
+        def bfs(x, y):
+            q = deque()
+            q.append((x, y, 1))
+            
+            delta = [(1, 0), (0, 1), (0, -1), (-1, 0),
+                    (1, 1), (-1, -1), (-1, 1), (1, -1)]
+            
+            visited = [[False] * len(grid) for _ in range(len(grid))]
+            visited[x][y] == True
+            
+            while q:
+                cur_x, cur_y, cur_d = q.popleft()
+                if cur_x == len(grid) - 1 and cur_y == len(grid) - 1:
+                        return cur_d
+                    
+                for dx, dy in delta:
+                    next_x, next_y = cur_x + dx, cur_y + dy
+                    if in_range(next_x, next_y) and not visited[next_x][next_y] and grid[next_x][next_y] == 0:
+                        q.append((next_x, next_y, cur_d + 1))
+                        visited[next_x][next_y] = True
+            return -1
+        
         
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
         
-        rows = len(grid)
-        cols = len(grid[0])
+        return bfs(0, 0)
         
-        def is_valid(x, y):
-            return 0 <= x < rows and 0 <= y < cols
         
-        def bfs():
-            q = deque()
-            visited = [[False] * cols for _ in range(rows)]
-            q.append((0, 0, 1))
-            visited[0][0] = True
-
-            delta = [(1, 0), (0, 1), (-1, 0), (0, -1),
-                    (1, 1), (-1, 1), (-1, -1), (1, -1)]
-            
-            while q:
- 
-                cur_x, cur_y, cur_d = q.popleft()
-                if cur_x == rows-1 and cur_y == cols-1:
-                    return cur_d
-                for dx, dy in delta:
-                    next_x, next_y = cur_x + dx, cur_y + dy
-                    if is_valid(next_x, next_y) and grid[next_x][next_y] == 0 and not visited[next_x][next_y]:
-                        q.append((next_x, next_y, cur_d + 1))
-                        visited[next_x][next_y] = True
-            
-            return -1
-
-        answer = bfs()
         
-        return answer

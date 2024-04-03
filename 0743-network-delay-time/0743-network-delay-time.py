e3a5aa77d6d78f499af4af0d1e51102985327884
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-03 13:56:56 +0900

    Time: 394 ms (23.66%), Space: 18.7 MB (20.07%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -6,26 +6,25 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         # 그래프 구현
         graph = defaultdict(list)
-        for time in times:
-            graph[time[0]].append((time[2], time[1]))
+        for start_v, end_v, cost in times:
+            graph[start_v].append((cost, end_v))
         
-        # 다익스트라로 최단거리 
-        costs = {}
+        # 다익스트라
         pq = []
-        heappush(pq,(0, k))
+        costs = {}
+        heappush(pq, (0, k))
         
         while pq:
             cur_cost, cur_node = heappop(pq)
             if cur_node not in costs:
                 costs[cur_node] = cur_cost
                 for cost, next_node in graph[cur_node]:
-                    next_cost = cur_cost + cost
-                    heappush(pq, (next_cost,  next_node))
-        
-        # 조건 확인
-        for i in range(1, n +1):
+                    next_cost = cost + cur_cost
+                    heappush(pq, (next_cost, next_node))
+        # 비교
+        for i in range(1, n+1):
             if i not in costs:
                 return -1
         
-        # 결과 반환
+        # 최대값 반환
         return max(costs.values())

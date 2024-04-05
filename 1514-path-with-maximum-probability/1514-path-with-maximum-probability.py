0265562214e7d7e336d1b3da995fc2b24aff1016
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-06 00:01:51 +0900

    Time: 540 ms (75.54%), Space: 29.8 MB (18.45%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -5,34 +5,31 @@ from heapq import heappush, heappop
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         graph = defaultdict(list)
-
+        
         for i in range(len(edges)):
             graph[edges[i][0]].append((edges[i][1], succProb[i]))
             graph[edges[i][1]].append((edges[i][0], succProb[i]))
-
+        
         pq = []
-        distances = [0] * n
-
+        probability = [0] * n
+        
         heappush(pq, (-1, start_node))
-
+        
         while pq:
             cur_prob, cur_node = heappop(pq)
             cur_prob *= -1
-
-            if distances[cur_node] >= cur_prob:
+            
+            if probability[cur_node] >= cur_prob:
                 continue
-
-            distances[cur_node] = cur_prob
-
+                
+            probability[cur_node] = cur_prob
+            
             if cur_node == end_node:
                 return cur_prob
-
-            for neighbor, prob in graph[cur_node]:
-                new_prob = cur_prob * prob
-
-                if distances[neighbor] < new_prob:
-                    heappush(pq, (-new_prob, neighbor))
-
-        return 0  
-
-
+            
+            for next_node, prob in graph[cur_node]:
+                next_prob = prob * cur_prob
+                if next_prob >= probability[next_node]:
+                    heappush(pq, (-next_prob, next_node))
+                    
+        return 0

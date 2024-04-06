ee7cf6323cb4222002e3be3c6afa676d63672f5b
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-07 00:38:28 +0900

    Time: 82 ms (92.96%), Space: 18.4 MB (67.39%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,12 +1,11 @@
 from collections import deque, defaultdict
 
-
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        visited = []
+        visited = [False] * numCourses
         graph = defaultdict(list)
+        answer = []
         indegree = [0] * numCourses
-    
         
         for v, u in prerequisites:
             graph[u].append(v)
@@ -16,18 +15,20 @@ class Solution:
         for v in range(numCourses):
             if indegree[v] == 0:
                 q.append(v)
+        temp = []
         
         while q:
             cur_course = q.popleft()
-            visited.append(cur_course)
+            visited[cur_course] = True
+            temp.append(cur_course)
             
             for next_v in graph[cur_course]:
                 indegree[next_v] -= 1
                 if indegree[next_v] == 0:
                     q.append(next_v)
         
-        if len(visited) != numCourses:
-            return []
+        if len(temp) == numCourses:
+            answer = temp
         
-        return visited
+        return answer
         

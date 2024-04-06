df2eba5e5eb64836b883d9a177a3c0fcfb526375
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-07 00:59:16 +0900

    Time: 251 ms (52.06%), Space: 18.9 MB (83.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,31 +3,36 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        n = len(grid[0])
+        m = len(grid)
+        visited = [[False] * n for _ in range(m)]
+        cnt = 0
+        
         def in_range(x, y):
-            return 0 <= x < len(grid) and 0 <= y < len(grid[0])
+            return 0 <= x < m and 0 <= y < n
         
         
         def bfs(x, y):
             q = deque()
             q.append((x, y))
-            visited[x][y] = True
             delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            visited[x][y] = True
             
             while q:
                 cur_x, cur_y = q.popleft()
+                
                 for dx, dy in delta:
                     next_x, next_y = cur_x + dx, cur_y + dy
                     if in_range(next_x, next_y) and not visited[next_x][next_y] and grid[next_x][next_y] == "1":
                         q.append((next_x, next_y))
                         visited[next_x][next_y] = True
-                        
         
-        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+    
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == "1" and not visited[i][j]:
                     bfs(i, j)
-                    count += 1
+                    cnt += 1
         
-        return count
+        return cnt
+    

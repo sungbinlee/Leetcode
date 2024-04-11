4cff144ac6e31f0c9ae43e348add329d972fbfec
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-11 23:23:23 +0900

    Time: 89 ms (61.98%), Space: 20.3 MB (46.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,4 +1,4 @@
-import collections
+from collections import defaultdict
 
 
 class Solution:
@@ -8,5 +8,4 @@ class Solution:
         for word in strs:
             anagrams[''.join(sorted(word))].append(word)
             
-        return list(anagrams.values())
-            
+        return anagrams.values()

50ee51a7839457220a2464ff485018d9f32b1a61
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-18 00:10:34 +0900

    Time: 34 ms (69.48%), Space: 16.4 MB (86.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,20 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = [[-1] * n for _ in range(m)]
+        memo = [[0] *n for _ in range(m)]
         
+        for r in range(m):
+            for c in range(n):
+                if r == 0 and c == 0:
+                    memo[r][c] = 1
+                else:
+                    memo[r][c] = memo[r-1][c] + memo [r][c-1]
         
-        def dfs(x, y):
-            if x == 0 and y == 0:
-                memo[x][y] = 1
-                return memo[x][y]
-            unique_path = 0
-            if memo[x][y] == -1:
-                if x-1 >= 0:
-                    unique_path += dfs(x-1, y)
-                if y-1 >= 0:
-                    unique_path += dfs(x, y-1)
-                memo[x][y] = unique_path
-
-            return memo[x][y]
-        
-        return dfs(m-1, n-1)
+        return memo[-1][-1]
+                

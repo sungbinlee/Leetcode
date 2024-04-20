fcaf67dc347b20c898d6635d0d1ac7688f76a980
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-20 22:32:20 +0900

    Time: 35 ms (51.82%), Space: 16.5 MB (83.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,29 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        memo = {}
+#         memo = {}
         
-        def cs(x):
-            if x == 1:
-                return 1
-            if x == 2:
-                return 2
+#         def cs(x):
+#             if x == 1:
+#                 return 1
+#             if x == 2:
+#                 return 2
             
-            if x not in memo:
-                memo[x] = cs(x-1) + cs(x-2)
+#             if x not in memo:
+#                 memo[x] = cs(x-1) + cs(x-2)
             
-            return memo[x]
+#             return memo[x]
         
-        return cs(n)
+#         return cs(n)
+        if n == 1:
+            return 1
+        
+        one_before = 1
+        two_before = 1
+        
+        for i in range(2, n+1):
+            total = one_before + two_before
+            two_before = one_before
+            one_before = total
+        
+        return total

6d54c5306b5eccb12426f80a3543df58ced6cc73
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-04-20 14:19:03 +0900

    Time: 744 ms (80.01%), Space: 17 MB (61.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,23 +3,32 @@ from collections import deque
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        answer = -1
-        depth = 0
+#         answer = -1
+#         depth = 0
         
-        visited = set()
+#         visited = set()
         
-        q = deque()
-        q.append((amount, depth))
+#         q = deque()
+#         q.append((amount, depth))
         
-        while q:
-            cur_amount, cur_depth = q.popleft()
-            if cur_amount == 0:
-                answer = cur_depth
-                break
-            for coin in coins:
-                next_amount = cur_amount - coin
-                if next_amount >= 0 and next_amount not in visited:
-                    q.append((next_amount, cur_depth + 1))
-                    visited.add(next_amount)
+#         while q:
+#             cur_amount, cur_depth = q.popleft()
+#             if cur_amount == 0:
+#                 answer = cur_depth
+#                 break
+#             for coin in coins:
+#                 next_amount = cur_amount - coin
+#                 if next_amount >= 0 and next_amount not in visited:
+#                     q.append((next_amount, cur_depth + 1))
+#                     visited.add(next_amount)
                     
-        return answer
+#         return answer
+
+        dp = [0] + [float("inf")] * amount
+        
+        for i in range(1, amount+1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        return -1 if dp[-1] == float("inf") else dp[-1]
